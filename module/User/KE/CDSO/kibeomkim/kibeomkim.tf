a8327ac157c7819545f5aa678fca303277723026
kibeom <siakan1107@gmail.com>
2023-10-17 21:58:37 +0900

    modify

--- module/User/KE/CDSO/kibeomkim/kibeomkim.tf
+++ module/User/KE/CDSO/kibeomkim/kibeomkim.tf
@@ -1,6 +1,6 @@
-
-resource "aws_identitystore_user" "user-kibeomkim1" {
-  identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0] 
+data "aws_ssoadmin_instances" "userkkb" {}
+resource "aws_identitystore_user" "userkibeomkim" {
+  identity_store_id = tolist(data.aws_ssoadmin_instances.userkkb.identity_store_ids)[0] 
 
   display_name = "testuser"
   user_name    = "testuser"
