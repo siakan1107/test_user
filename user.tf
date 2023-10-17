2a472cc6babb2281f60742b80db49cdcf0d25ffc
kibeom <siakan1107@gmail.com>
2023-10-17 18:06:24 +0900

    modify

--- user.tf
+++ user.tf
@@ -1,35 +1,26 @@
 data "aws_ssoadmin_instances" "user-kibeomkim" {}
 
-# Create SSO user1
-resource "aws_identitystore_user" "user-kibeomkim2" {
-  identity_store_id = tolist(data.aws_ssoadmin_instances.user-kibeomkim.identity_store_ids)[0] 
-
-  display_name = "test"
-  user_name    = "test"
-
-  name {
-    given_name  = "test"
-    family_name = "test"
-  }
-
-  emails {
-    value = "testkkb@koreanair.com"  # Replace with your email ID
-  }
-
+locals {
+  
+  given_name = ["test","test1"]
+  display_name = ["test","test1"]
+  user_name = ["test","test1"]
+  family_name =["test","test1"]
 }
-  resource "aws_identitystore_user" "user-kibeomkim2" {
-  identity_store_id = tolist(data.aws_ssoadmin_instances.user-kibeomkim.identity_store_ids)[0] 
 
-  display_name = "test2"
-  user_name    = "test2"
+# Create SSO user1
+resource "aws_identitystore_user" "user-kibeomkim3" {
+    identity_store_id = tolist(data.aws_ssoadmin_instances.user-kibeomkim.identity_store_ids)[0] 
 
-  name {
-    given_name  = "test2"
-    family_name = "test2"
-  }
+    user_name = [local.user_name]
+    display_name = [local.display_name]
+ name{
+    given_name = [local.given_name]
+    family_name = [local.family_name]
+    }
+ 
+ 
 
-  emails {
-    value = "testkkb2@koreanair.com"  # Replace with your email ID
-  }
+
 }
 
