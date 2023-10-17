d56a747fe79ae72757bcc648605aa8f453e63bfb
kibeom <siakan1107@gmail.com>
2023-10-17 21:56:10 +0900

    test

--- module/User/KE/CDSO/kibeomkim/kibeomkim.tf
+++ module/User/KE/CDSO/kibeomkim/kibeomkim.tf
@@ -1,6 +1,6 @@
 
 resource "aws_identitystore_user" "user-kibeomkim1" {
-  identity_store_id = tolist(data.aws_ssoadmin_instances.user-kibeomkim.identity_store_ids)[0] 
+  identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0] 
 
   display_name = "testuser"
   user_name    = "testuser"
