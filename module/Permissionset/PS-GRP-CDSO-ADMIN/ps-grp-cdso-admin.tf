6d87c43c9eaf164742374b9790c32bf05693d3f4
kibeom <siakan1107@gmail.com>
2023-10-18 00:01:35 +0900

    modify

--- module/Permissionset/PS-GRP-CDSO-ADMIN/ps-grp-cdso-admin.tf
+++ module/Permissionset/PS-GRP-CDSO-ADMIN/ps-grp-cdso-admin.tf
@@ -11,23 +11,11 @@ resource "aws_ssoadmin_managed_policy_attachment" "ec2_admin_managed_policy_atta
   permission_set_arn = aws_ssoadmin_permission_set.ps-grp-cdso-admin-managed.arn
 }
 
-data "aws_iam_policy_document" "inline-policy" {
-  statement {
-    sid = "1"
 
-    actions = [
-      "s3:ListAllMyBuckets",
-      "s3:GetBucketLocation",
-    ]
-
-    resources = [
-      "arn:aws:s3:::*",
-    ]
-  }
-}
 
 resource "aws_ssoadmin_permission_set_inline_policy" "inline" {
-  inline_policy      = data.aws_iam_policy_document.inline-policy.json
+  #inline_policy      = data.aws_iam_policy_document.inline-policy.json
+  inline_policy      = file("../ps-grp-cdso-admin.json")
   instance_arn       = tolist(data.aws_ssoadmin_instances.ps-grp-cdso-admin.arns)[0]
   permission_set_arn = aws_ssoadmin_permission_set.ps-grp-cdso-admin-managed.arn
 }
