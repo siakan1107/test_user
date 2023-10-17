d70c89c73ca0ef7ee7d166364aa7dcf8160341dc
kibeom <siakan1107@gmail.com>
2023-10-17 23:02:00 +0900

    modify

--- module/Group/groupmain.tf
+++ module/Group/groupmain.tf
@@ -1,15 +1,15 @@
 module "grp-cdso-admin" {
   source = "./CDSO/"
 
-  display_name = "GRP-CDSO-ADMIN"
-  description = "This is GRP-CDSO-ADMIN GROUP"
+  display_name = "GRP-CDSO-ADMIN1"
+  description = "This is GRP-CDSO-ADMIN1 GROUP"
 
 }
 
 module "grp-pss-pns-admin" {
   source = "./PSS/"
 
-  display_name = "GRP-PSS-PNS-ADMIN"
-  description = "This is GRP-PSS-PNS-ADMIN"
+  display_name = "GRP-PSS-PNS-ADMIN1"
+  description = "This is GRP-PSS-PNS-ADMIN1"
 
 }
