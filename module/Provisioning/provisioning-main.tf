cecf89100d6c32bea728dc87426b149c1ca37fea
kibeom <siakan1107@gmail.com>
2023-10-18 01:37:26 +0900

    test

--- module/Provisioning/provisioning-main.tf
+++ module/Provisioning/provisioning-main.tf
@@ -1,8 +1,9 @@
 module "pv-ps-grp-cdso-admin" {
   source = "./provisioning"
 
-    permission_set_arn = module.ps-grp-cdso-admin-managed.ps-grp-cdso-admin-arn
+    permission_set_arn = module.ps-grp-cdso-admin.ps-grp-cdso-admin-arn
     principal_id = module.grp-cdso-admin.grp-cdso-admin-id
+
     principal_type = "GROUP"
     target_id = "472318675080"
     target_type = "AWS_ACCOUNT"
@@ -10,8 +11,9 @@ module "pv-ps-grp-cdso-admin" {
 module "pv-ps-grp-pss-pns-admin" {
   source = "./provisioning/"
 
-    permission_set_arn = module.ps-grp-pss-pns-admin-managed.ps-grp-pss-pns-admin-arn
+    permission_set_arn = module.ps-grp-pss-pns-admin.ps-grp-pss-pns-admin-arn
     principal_id = module.grp-pss-pns-admin.grp-pss-pns-admin-id
+
     principal_type = "GROUP"
     target_id = "472318675080"
     target_type = "AWS_ACCOUNT"
