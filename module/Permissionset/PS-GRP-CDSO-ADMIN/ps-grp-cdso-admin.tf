a8b23e8267fce4ec318350ceac3bbf72ca091cab
kibeom <siakan1107@gmail.com>
2023-10-18 00:11:58 +0900

    test

--- module/Permissionset/PS-GRP-CDSO-ADMIN/ps-grp-cdso-admin.tf
+++ module/Permissionset/PS-GRP-CDSO-ADMIN/ps-grp-cdso-admin.tf
@@ -14,7 +14,7 @@ resource "aws_ssoadmin_managed_policy_attachment" "ec2_admin_managed_policy_atta
 
 resource "aws_ssoadmin_permission_set_inline_policy" "inline" {
   #inline_policy      = data.aws_iam_policy_document.inline-policy.json
-  inline_policy      = file("../inlinepolicy/ps-grp-cdso-admin.json")
+  inline_policy      = file("/ps-grp-cdso-admin.json")
   instance_arn       = tolist(data.aws_ssoadmin_instances.ps-grp-cdso-admin.arns)[0]
   permission_set_arn = aws_ssoadmin_permission_set.ps-grp-cdso-admin-managed.arn
 }
