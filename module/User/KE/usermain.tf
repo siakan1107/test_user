c23a9561fd389ed59b9a849506742a36f48bb8aa
kibeom <siakan1107@gmail.com>
2023-10-17 22:33:53 +0900

    test

--- module/User/KE/usermain.tf
+++ module/User/KE/usermain.tf
@@ -1,10 +1,21 @@
 module "user-cdso-module" {
   source = "./CDSO/"
 
-  display_name = "testcoso"
-  user_name    = "testcoso"
-  given_name  = "testcoso"
-  family_name = "testcoso"
+  display_name = "testcdso"
+  user_name    = "testcdso"
+  given_name  = "testcdso"
+  family_name = "testcdso"
   emails = "testcoso@testuser1.com"
+
+}
+
+module "user-cdso-module" {
+  source = "./CDSO/"
+
+  display_name = "testcdso2"
+  user_name    = "testcdso2"
+  given_name  = "testcdso2"
+  family_name = "testcdso2"
+  emails = "testcdso2@testuser1.com"
 
 }
