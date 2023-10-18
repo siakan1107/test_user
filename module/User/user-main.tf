889d0ddb213a6c5f7e0d8d906e1fe15f751af678
kibeom <siakan1107@gmail.com>
2023-10-18 10:40:08 +0900

    test

--- module/User/user-main.tf
+++ module/User/user-main.tf
@@ -16,6 +16,6 @@ module "user-testcdso2" {
   user_name    = "testcdso2"
   given_name  = "testcdso2"
   family_name = "testcdso2"
-  emails = "testcoso@testuser1.com"
+  emails = "testcoso2@testuser2.com"
 
 }
