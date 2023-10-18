19ab92879b1f2ce3d8b55d875a0b0c5036cb5bd5
kibeom <siakan1107@gmail.com>
2023-10-18 14:50:38 +0900

    test

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 module "user" {
-  source = "./module/Main/User"
+  source = "./module/User/"
+
+  display_name = "Kimtest"
+  user_name    = "Kimtest"
+  given_name  = "Kibeomkimtest"
+  family_name = "Kim"
+  emails = "testusekim@testuser1.com"
 
 }
