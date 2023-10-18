8a2e5afdd760675c7614ab7c6cbbfe7d0a3e8c99
kibeom <siakan1107@gmail.com>
2023-10-18 15:19:34 +0900

    test

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
 module "user" {
-  source = "./module/User/"
-
-  display_name = "Kimtesttest"
-  user_name    = "Kimtesttest"
-  given_name  = "Kibeomkimtest"
-  family_name = "Kim"
-  emails = "testusekim2@testuser1.com"
-
-
+  source = "./module/KE/CDSO/"
 
 }
---
+++ module/KE/CDSO/kibeomkim.tf
@@ -0,0 +1,12 @@
+module "user-kibeomkim" {
+  source = "../User/"
+
+  display_name = "Kimtesttest"
+  user_name    = "Kimtesttest"
+  given_name  = "Kibeomkimtest"
+  family_name = "Kim"
+  emails = "testusekim2@testuser1.com"
+
+
+
+}
